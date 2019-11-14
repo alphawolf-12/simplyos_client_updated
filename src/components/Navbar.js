ce8180eddc3edf85699976bb84353de9bd2db15c
Redus Rira <redis123445@gmail.com>
2019-11-14 20:17:44 +0100

    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,13 +24,6 @@ export default class Navbar  extends Component {
 						<div class="row">
 						<div class="col-xs-12 text-right">
 							<p class="site">www.simplyopensource.in</p>
-							<p class="num">Call: +01 234 567 890</p>
-							<ul class="fh5co-social">
-							<li><a href="#"><i class="icon-facebook2"></i></a></li>
-							<li><a href="#"><i class="icon-twitter2"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble2"></i></a></li>
-							<li><a href="#"><i class="icon-github"></i></a></li>
-							</ul>
 						</div>
 						</div>
 					</div>
